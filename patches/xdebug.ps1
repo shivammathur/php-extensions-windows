a73510fde891a3b328e7111be6b22e6c4a4b4cd4
Shivam Mathur <shivam_jpr@hotmail.com>
2025-10-14 00:03:24 +0000

    Add patch to fix maps.c in Xdebug

--- patches/xdebug.ps1
+++ patches/xdebug.ps1
@@ -8,3 +8,19 @@ param(
 if($php -match '8.6') {
     (Get-Content "$path\config.w32" -Raw) -replace ([regex]::Escape('80600')), '80700' | Set-Content "$path\config.w32" -Encoding UTF8
 }
+
+if($php -match '8.[5-6]') {
+    $maps = "$path\src\lib\maps\maps.c"
+    $content = Get-Content $maps -Raw -Encoding UTF8
+    $patched = [regex]::Replace($content,
+        '(^\s*#\s*include\s*<stdlib\.h>\s*\r?\n)(?!\s*#\s*include\s*"php\.h")',
+        '$1#include "php.h"' + "`r`n",
+        [System.Text.RegularExpressions.RegexOptions]::Multiline)
+    $patched = [regex]::Replace(
+    $patched,
+    '\bcase\s+GLOB_NOMATCH\s*:',
+    'case PHP_GLOB_NOMATCH:',
+    [System.Text.RegularExpressions.RegexOptions]::IgnoreCase
+    )
+    if ($patched -ne $content) { Set-Content $maps -Value $patched -Encoding UTF8 }
+}
